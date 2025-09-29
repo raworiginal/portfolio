8fc663dd8e41c1825892cd6738981c2d35fb8406
raworiginal <118763950+raworiginal@users.noreply.github.com>
2025-09-29 15:07:06 -0700

    completed landing page

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -4,42 +4,52 @@ function Landing() {
 			<div className="hero-content flex-col lg:flex-row">
 				<img
 					src="https://avatars.githubusercontent.com/u/118763950?s=400&u=b88d72061ef146431818c2662dc366a99f3d9ab1&v=4"
-					className="max-w-sm rounded-full shadow-2xl"
+					className="grow-0 max-w-sm rounded-full shadow-2xl"
 				/>
-				<div className="">
-					<h1 className="text-5xl font-bold">Randall Wolfe</h1>
-					<h2 className="text-3xl font-bold">Full-Stack Engineer</h2>
-					<div className="divider divider-secondary"></div>
-					<p className="">Intuitive Frontend Experiences</p>
-					<p className="">Scalable Backend Systems</p>
-					<div className="divider divider-secondary"></div>
-					<button className="btn bg-black text-white border-black">
-						<svg
-							aria-label="GitHub logo"
-							width="32"
-							height="32"
-							xmlns="http://www.w3.org/2000/svg"
-							viewBox="0 0 24 24">
-							<path
-								fill="white"
-								d="M12,2A10,10 0 0,0 2,12C2,16.42 4.87,20.17 8.84,21.5C9.34,21.58 9.5,21.27 9.5,21C9.5,20.77 9.5,20.14 9.5,19.31C6.73,19.91 6.14,17.97 6.14,17.97C5.68,16.81 5.03,16.5 5.03,16.5C4.12,15.88 5.1,15.9 5.1,15.9C6.1,15.97 6.63,16.93 6.63,16.93C7.5,18.45 8.97,18 9.54,17.76C9.63,17.11 9.89,16.67 10.17,16.42C7.95,16.17 5.62,15.31 5.62,11.5C5.62,10.39 6,9.5 6.65,8.79C6.55,8.54 6.2,7.5 6.75,6.15C6.75,6.15 7.59,5.88 9.5,7.17C10.29,6.95 11.15,6.84 12,6.84C12.85,6.84 13.71,6.95 14.5,7.17C16.41,5.88 17.25,6.15 17.25,6.15C17.8,7.5 17.45,8.54 17.35,8.79C18,9.5 18.38,10.39 18.38,11.5C18.38,15.32 16.04,16.16 13.81,16.41C14.17,16.72 14.5,17.33 14.5,18.26C14.5,19.6 14.5,20.68 14.5,21C14.5,21.27 14.66,21.59 15.17,21.5C19.14,20.16 22,16.42 22,12A10,10 0 0,0 12,2Z"></path>
-						</svg>
-						Github
-					</button>
-					<button className="btn bg-[#0967C2] text-white border-[#0059b3]">
-						<svg
-							aria-label="LinkedIn logo"
-							width="32"
-							height="32"
-							xmlns="http://www.w3.org/2000/svg"
-							viewBox="0 0 32 32">
-							<path
-								fill="white"
-								d="M26.111,3H5.889c-1.595,0-2.889,1.293-2.889,2.889V26.111c0,1.595,1.293,2.889,2.889,2.889H26.111c1.595,0,2.889-1.293,2.889-2.889V5.889c0-1.595-1.293-2.889-2.889-2.889ZM10.861,25.389h-3.877V12.87h3.877v12.519Zm-1.957-14.158c-1.267,0-2.293-1.034-2.293-2.31s1.026-2.31,2.293-2.31,2.292,1.034,2.292,2.31-1.026,2.31-2.292,2.31Zm16.485,14.158h-3.858v-6.571c0-1.802-.685-2.809-2.111-2.809-1.551,0-2.362,1.048-2.362,2.809v6.571h-3.718V12.87h3.718v1.686s1.118-2.069,3.775-2.069,4.556,1.621,4.556,4.975v7.926Z"
-								fillRule="evenodd"></path>
-						</svg>
-						LinkedIn
-					</button>
+				<div className="w-full flex flex-col flex-1">
+					<h1 className="text-center text-5xl font-extrabold">Randall Wolfe</h1>
+					<h2 className="text-center text-3xl font-extrabold">
+						Full-Stack Engineer
+					</h2>
+					<div className="divider divider-secondary m-1"></div>
+					<div className="flex justify-center">
+						<div className="flex flex-col">
+							<p className="text-2xl font-bold">
+								Intuitive Frontend Experiences
+							</p>
+							<p className="text-2xl font-bold">Scalable Backend Systems</p>
+						</div>
+					</div>
+					<div className="divider divider-secondary m-1"></div>
+					<div className="flex justify-center gap-1">
+						<button className="btn bg-black text-white border-black">
+							<svg
+								aria-label="GitHub logo"
+								width="32"
+								height="32"
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 24 24">
+								<path
+									fill="white"
+									d="M12,2A10,10 0 0,0 2,12C2,16.42 4.87,20.17 8.84,21.5C9.34,21.58 9.5,21.27 9.5,21C9.5,20.77 9.5,20.14 9.5,19.31C6.73,19.91 6.14,17.97 6.14,17.97C5.68,16.81 5.03,16.5 5.03,16.5C4.12,15.88 5.1,15.9 5.1,15.9C6.1,15.97 6.63,16.93 6.63,16.93C7.5,18.45 8.97,18 9.54,17.76C9.63,17.11 9.89,16.67 10.17,16.42C7.95,16.17 5.62,15.31 5.62,11.5C5.62,10.39 6,9.5 6.65,8.79C6.55,8.54 6.2,7.5 6.75,6.15C6.75,6.15 7.59,5.88 9.5,7.17C10.29,6.95 11.15,6.84 12,6.84C12.85,6.84 13.71,6.95 14.5,7.17C16.41,5.88 17.25,6.15 17.25,6.15C17.8,7.5 17.45,8.54 17.35,8.79C18,9.5 18.38,10.39 18.38,11.5C18.38,15.32 16.04,16.16 13.81,16.41C14.17,16.72 14.5,17.33 14.5,18.26C14.5,19.6 14.5,20.68 14.5,21C14.5,21.27 14.66,21.59 15.17,21.5C19.14,20.16 22,16.42 22,12A10,10 0 0,0 12,2Z"></path>
+							</svg>
+							Github
+						</button>
+						<button className="btn bg-[#0967C2] text-white border-[#0059b3]">
+							<svg
+								aria-label="LinkedIn logo"
+								width="32"
+								height="32"
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 32 32">
+								<path
+									fill="white"
+									d="M26.111,3H5.889c-1.595,0-2.889,1.293-2.889,2.889V26.111c0,1.595,1.293,2.889,2.889,2.889H26.111c1.595,0,2.889-1.293,2.889-2.889V5.889c0-1.595-1.293-2.889-2.889-2.889ZM10.861,25.389h-3.877V12.87h3.877v12.519Zm-1.957-14.158c-1.267,0-2.293-1.034-2.293-2.31s1.026-2.31,2.293-2.31,2.292,1.034,2.292,2.31-1.026,2.31-2.292,2.31Zm16.485,14.158h-3.858v-6.571c0-1.802-.685-2.809-2.111-2.809-1.551,0-2.362,1.048-2.362,2.809v6.571h-3.718V12.87h3.718v1.686s1.118-2.069,3.775-2.069,4.556,1.621,4.556,4.975v7.926Z"
+									fillRule="evenodd"></path>
+							</svg>
+							LinkedIn
+						</button>
+					</div>
 				</div>
 			</div>
 		</div>
