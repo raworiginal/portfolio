bdb87be8373b90853f54252562c5f977b9740f29
raworiginal <118763950+raworiginal@users.noreply.github.com>
2025-09-30 07:31:36 -0700

    Completed sticky functionality for the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import Landing from "./components/Landing";
 import Navbar from "./components/Navbar";
+import About from "./components/About";
+import Projects from "./components/Projects";
 function App() {
 	return (
 		<>
 			<Navbar />
 			<Landing />
+			<About />
 		</>
 	);
 }
---
+++ src/components/Projects.jsx
@@ -0,0 +1,2 @@
+function Projects() {}
+export default Projects;
