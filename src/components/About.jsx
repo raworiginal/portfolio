371a8317f586b4458f9d5c549aa070f69d96a94e
raworiginal <118763950+raworiginal@users.noreply.github.com>
2025-09-30 09:24:53 -0700

    completed about me componenet

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,24 @@
 function About() {
-	return <h1>About Me</h1>;
+	return (
+		<div className="flex flex-col pt-6 px-6 mx-auto max-w-xl">
+			<h1 className="text-5xl font-bold">About Me</h1>
+			<p className="pt-4 text-justify">
+				Full-stack software engineer specializing in UX/UI development. I
+				combine 12 years of Teaching and IT management with expertise in
+				MongoDB, Express, React, and Node to create responsive, user-focused
+				applications. Adept at simplifying complex workflows into seamless
+				digital experiences and fostering team collaboration to deliver
+				high-impact products.
+			</p>
+			<p className="pt-4 text-justify">
+				Former Computer Science Teacher with a proven record of increasing
+				student engagement and outcomes, including raising AP Computer Science
+				Principles pass rates by 20 percentage points. Passionate about building
+				user-focused applications and empowering teams to create impactful
+				digital solutions. Skilled in integrating technical fluency and design
+				principles to deliver seamless and engaging user experiences.
+			</p>
+		</div>
+	);
 }
 export default About;
